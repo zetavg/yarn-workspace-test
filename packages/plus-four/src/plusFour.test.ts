7b683c5def67c59910a445262e0014dc0a70c073
zetavg <mail@zeta.vg>
2024-02-10 03:32:12 +0800

    Revert "test ci"
    This reverts commit 0a13da9c9ab801309fc2e81d187de49df1b74138.

--- packages/plus-four/src/plusFour.test.ts
+++ packages/plus-four/src/plusFour.test.ts
@@ -2,5 +2,4 @@ import plusFour from './plusFour';
 
 test('1 plus four equals 5', () => {
   expect(plusFour(1)).toBe(5);
-  expect(true).toBe(false);
 });
--- packages/plus-two/src/plusTwo.ts
+++ packages/plus-two/src/plusTwo.ts
@@ -1,7 +1,5 @@
 import sum from '@yarn-workspace-test/sum';
 
 export default function plusTwo(a: number) {
-  return sum(a, 2 as string);
+  return sum(a, 2);
 }
-
-const a: number = '123';
